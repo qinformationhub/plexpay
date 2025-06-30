3437f2c990d6fc3e7ecdab0919d1462c27ef26c5
SIBTAIN-ASAD <sibtainasad50@gmail.com>
2025-07-01 03:21:29 +0500

    update socket for serveless

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
-import { setupVite, serveStatic, log } from "./vite";
+import { serveStatic, log } from "./vite";
 
 const app = express();
 app.use(express.json());
@@ -37,31 +37,26 @@ app.use((req, res, next) => {
   next();
 });
 
-(async () => {
-  const server = await registerRoutes(app);
+registerRoutes(app);
 
-  app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
-    const status = err.status || err.statusCode || 500;
-    const message = err.message || "Internal Server Error";
-
-    res.status(status).json({ message });
-    throw err;
-  });
-
-  // importantly only setup vite in development and after
-  // setting up all the other routes so the catch-all route
-  // doesn't interfere with the other routes
-  if (app.get("env") === "development") {
-    await setupVite(app, server);
-  } else {
-    serveStatic(app);
+app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
+  const status = err.status || err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(status).json({ message });
+  if (process.env.NODE_ENV !== "production") {
+    console.error(err);
   }
+});
 
-  // ALWAYS serve the app on port 5001
-  // this serves both the API and the client.
-  // It is the only port that is not firewalled.
+if (process.env.NODE_ENV === "production") {
+  serveStatic(app);
+}
+
+export default app;
+
+if (process.env.NODE_ENV !== "production") {
   const port = process.env.PORT || 3000;
-  server.listen(port, () => {
+  app.listen(port, () => {
     log(`serving on port ${port}`);
   });
-})();
+}
